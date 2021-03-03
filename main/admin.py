469a3b4bc6c7e6099956e97fb85868f12343b0bd
medson <medsonnaftal@gmail.com>
2021-03-03 10:36:38 +0300

    sdghsgdhsgd

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import School, SubjectCombination, SchoolSubject, ExamResult, ExamRank
 
 # Register your models here.
+admin.site.register(School)
+admin.site.register(SubjectCombination)
+admin.site.register(SchoolSubject)
+admin.site.register(ExamResult)
+admin.site.register(ExamRank)
